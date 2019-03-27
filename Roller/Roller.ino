cc7bf8c2635acec6787122bf9663fcb931c1f968
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-03-27 18:27:08 -0400

    Add button pin values

--- Roller/Roller.ino
+++ Roller/Roller.ino
@@ -2,28 +2,32 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 long randNumber;
+const int buttonPin = A0;
+const int button2 = A1;
+const int button3 = A2;
+const int button4 = A3;
+int buttonState = 0;
 
 void setup() {
   lcd.begin(16, 2); //initializes the lcd as output
   randomSeed(analogRead(0)); //
   Serial.setTimeout(10);
   randNumber = random(1, 7); //sets the range or number of sides on the die
-  pinMode(7, INPUT); //one die
+  pinMode(buttonPin, INPUT); //one die
   pinMode(8, INPUT); //two dice
-  lcd.println("Select # of dice to roll");
+  lcd.println("Select # of dice <<end1 to roll");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-{ if (digitalRead(7) == HIGH) {
-      lcd.setCursor(0, 0);
-      lcd.print("");
-      lcd.println(randNumber);
+  buttonState = digitalRead(buttonPin);
 
+{ if (digitalRead(buttonPin) == LOW) {
+      lcd.println(randNumber);
+      lcd.println("Roll Again");
     }
   }
-
 
 
 }
